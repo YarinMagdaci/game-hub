6cc68ff6a6c9f422d7d3208d47b99f5064819f52
YarinMagdaci <yarin0600@gmail.com>
2023-03-29 15:13:48 +0300

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from '../assets/No_image_available.png';
 const getCroppedImageUrl = (url: string) => {
-   if(!url) return '';
+   if(!url) return noImage;
    const target = 'media/';
    const idx = url.indexOf(target) + target.length; // https://...media/
    return url.slice(0, idx) + 'crop/600/400/' + url.slice(idx);
