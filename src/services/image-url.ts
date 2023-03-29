b8b559cdf0f6e9d61b90aab7e0b2c962c5d30af9
YarinMagdaci <yarin0600@gmail.com>
2023-03-29 12:11:24 +0300

    Show loading skeletons

---
+++ src/services/image-url.ts
@@ -0,0 +1,7 @@
+const getCroppedImageUrl = (url: string) => {
+   const target = 'media/';
+   const idx = url.indexOf(target) + target.length; // https://...media/
+   return url.slice(0, idx) + 'crop/600/400/' + url.slice(idx);
+}
+
+export default getCroppedImageUrl;
